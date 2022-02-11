ad3f29f52d52cda4180e079a32f9cf4425a121db
WilliamMollet <99485441+WilliamMollet@users.noreply.github.com>
2022-02-11 15:40:50 +0100

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,37 @@
+var text_slam = document.querySelector("#SLAM");
+var text_sisr = document.querySelector("#SISR");
+var text_cyber = document.querySelector("#CYBER");
+
+function Slam() {
+    if (text_slam.style.display === "block") {
+        text_slam.style.display = "none";
+    } else {
+        text_slam.style.display = "block";
+    }
+    text_sisr.style.display = "none"
+    text_cyber.style.display = "none"
+}
+
+function SISR() {
+    if (text_sisr.style.display === "block") {
+        text_sisr.style.display = "none";
+    } else {
+        text_sisr.style.display = "block";
+    }
+    text_slam.style.display = "none"
+    text_cyber.style.display = "none"
+}
+
+function Cyber() {
+    if (text_cyber.style.display === "block") {
+        text_cyber.style.display = "none";
+    } else {
+        text_cyber.style.display = "block";
+    }
+    text_sisr.style.display = "none"
+    text_slam.style.display = "none"
+}
+
+function mail() {
+    navigator.clipboard.writeText("williammdev@gmail.com");
+}
